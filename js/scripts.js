83566c18e1bdde28c2913ab94967b4d6dcbe5223
Eric Swotinsky and Jamie Pittak <eric@>
2018-01-24 10:34:07 -0800

    add o functionality and prevent override

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,37 @@
 $(document).ready(function(){
 
 
+  var player1 = "x"
+  var player2 = "o"
+  var player = player1;
 
-  $(".col-md-4").click(function(){
-    $(this).children().text("x");
-
+  $(".changePlayer").click(function(){
+    if (player == player1) {
+      player = player2;
+    }
+    else if (player == player2) {
+      player = player1;
+    }
   })
 
+
+  $(".col-md-4").click(function(){
+
+
+    if(player == player1 && $(this).children()[0].innerText == player2){
+      alert("try again");
+    }
+
+    else if (player == player2 && $(this).children()[0].innerText == player1) {
+      alert("try again");
+    }
+
+    else {
+      $(this).children().text(player);
+     
+
+    }
+
+    })
 
 })
