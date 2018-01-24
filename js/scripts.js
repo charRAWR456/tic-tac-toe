a8b90752c05b39b76b61e4a3bad46510b9674922
Eric Swotinsky and Jamie Pittak <jamie@>
2018-01-24 13:29:55 -0800

    add win condition check and automatic player switch

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,100 @@
 //back end
 
+function changePlayer() {
+  if (player == "X") {
+    player = "O";
+  }
+  else if (player == "O") {
+    player = "X";
+  }
+  $("#currentPlayer").text(player);
+}
+
+function resetGame() {
+  $(".col-md-4").children().text("");
+  player = "X";
+  $("#currentPlayer").text(player);
+  $("#winner").text("");
+}
+
+
+function winCheck() {
+
+  if ($("#top-left").children()[0].innerText == player && $("#mid-left").children()[0].innerText == player && $("#bot-left").children()[0].innerText == player) {
+    $("#winner").text("YOU WIN!!!!!!!");
+  }
+
+  else if ($("#top-center").children()[0].innerText == player && $("#mid-center").children()[0].innerText == player && $("#bot-center").children()[0].innerText == player) {
+    $("#winner").text("YOU WIN!!!!!!!");
+  }
+
+  else if ($("#top-right").children()[0].innerText == player && $("#mid-right").children()[0].innerText == player && $("#bot-right").children()[0].innerText == player) {
+    $("#winner").text("YOU WIN!!!!!!!");
+  }
+
+  else if ($("#top-left").children()[0].innerText == player && $("#top-center").children()[0].innerText == player && $("#top-right").children()[0].innerText == player) {
+    $("#winner").text("YOU WIN!!!!!!!");
+  }
+
+  else if ($("#mid-left").children()[0].innerText == player && $("#mid-center").children()[0].innerText == player && $("#mid-right").children()[0].innerText == player) {
+    $("#winner").text("YOU WIN!!!!!!!");
+  }
+
+  else if ($("#bot-left").children()[0].innerText == player && $("#bot-center").children()[0].innerText == player && $("#bot-right").children()[0].innerText == player) {
+    $("#winner").text("YOU WIN!!!!!!!");
+  }
+
+  else if ($("#top-left").children()[0].innerText == player && $("#mid-center").children()[0].innerText == player && $("#bot-right").children()[0].innerText == player) {
+    $("#winner").text("YOU WIN!!!!!!!");
+  }
+
+  else if ($("#top-right").children()[0].innerText == player && $("#mid-center").children()[0].innerText == player && $("#bot-left").children()[0].innerText == player) {
+    $("#winner").text("YOU WIN!!!!!!!");
+  }
+}
+
 
 
 
 //front end
 $(document).ready(function(){
 
-
-  var player1 = "x"
-  var player2 = "o"
-  var player = player1;
+  player= "X";
+  $("#currentPlayer").text(player);
 
   $(".changePlayer").click(function(){
-    if (player == player1) {
-      player = player2;
-    }
-    else if (player == player2) {
-      player = player1;
-    }
+    changePlayer();
   })
-
 
   $(".col-md-4").click(function(){
 
-
-    if(player == player1 && $(this).children()[0].innerText == player2){
+    if(player == "X" && $(this).children()[0].innerText == "O"){
       alert("try again");
     }
-
-    else if (player == player2 && $(this).children()[0].innerText == player1) {
+    else if (player == "O" && $(this).children()[0].innerText == "X") {
       alert("try again");
     }
+    else if(player == "X" && $(this).children()[0].innerText == "X"){
 
+    }
+    else if (player == "O" && $(this).children()[0].innerText == "O") {
+
+    }
     else {
       $(this).children().text(player);
-     
-
+      winCheck();
+      changePlayer();
     }
 
-    })
 
+
+
+
+  })
+
+
+
+  $(".resetGame").click(function(){
+    resetGame();
+  })
 })
